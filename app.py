fb1c5a5238c55bf280546d6a3676bc45e2af836d
chaudharyhimanshu <hchaudhary_be17@thapar.edu>
2020-08-08 22:01:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,16 +4,22 @@ Created on Sat Aug  8 17:16:42 2020
 
 @author: Himanshu
 """
-
+''' I uesd the flask for creating the web application'''
 from flask import Flask, request, render_template
 import pickle
+
+'''app will be the name of app that we will be giving with writing Procfile'''
 
 app = Flask(__name__)
 model = pickle.load(open('graph.pkl', 'rb'))
 
+'''theis will be home page of the web application'''
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+'''this will be the page where recommended tags'''
 
 @app.route('/recommend',methods=['POST'])
 def predict():
